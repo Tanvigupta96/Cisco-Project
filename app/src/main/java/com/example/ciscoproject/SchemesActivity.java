350f863e0c27164161609f94975a4cda7e3e4d1a
Tanvigupta96 <guptatannu.34gmail.com>
2019-02-03 21:17:06 +0530

    login added

---
+++ app/src/main/java/com/example/ciscoproject/SchemesActivity.java
@@ -0,0 +1,13 @@
+package com.example.ciscoproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SchemesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schemes);
+    }
+}
---
+++ app/src/main/java/com/example/ciscoproject/SplashScreenActivity.java
@@ -0,0 +1,49 @@
+package com.example.ciscoproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    Thread splashThread;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        splashThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+                    // Splash screen pause time
+                    while (waited < 3500) {
+                        sleep(100);
+                        waited += 100;
+                    }
+
+
+                    loadMainActivity();
+                } catch (InterruptedException e) {
+                    // do nothing
+                } finally {
+//                    SplashActivity.this.finish();
+
+                }
+
+            }
+        };
+        splashThread.start();
+    }
+
+    public void loadMainActivity()
+    {
+        Intent pintent = new Intent(SplashScreenActivity.this,SchemesActivity.class);
+        pintent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        pintent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        pintent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        startActivity(pintent);
+    }
+
+    }
+
