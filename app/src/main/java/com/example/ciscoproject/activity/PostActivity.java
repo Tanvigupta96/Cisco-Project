614c24c17f230d192aca6514790a76471b3f6801
koshimagoyal <koshimagoyal@yahoo.co.in>
2019-02-05 09:58:44 +0530

    firebase

--- app/src/main/java/com/example/ciscoproject/activity/PostActivity.java
+++ app/src/main/java/com/example/ciscoproject/activity/PostActivity.java
@@ -34,6 +34,7 @@ public class PostActivity extends AppCompatActivity {
     private Button postBtn; private StorageReference storage; private FirebaseDatabase database;
     private DatabaseReference databaseRef; private FirebaseAuth mAuth;
     private DatabaseReference mDatabaseUsers; private FirebaseUser mCurrentUser;
+    private Uri filePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +44,7 @@ public class PostActivity extends AppCompatActivity {
         textDesc = (EditText)findViewById(R.id.textDesc);
         textTitle = (EditText)findViewById(R.id.textTitle);
         storage = FirebaseStorage.getInstance().getReference();
-        databaseRef = database.getInstance().getReference().child("image");
+        databaseRef = database.getInstance().getReferenceFromUrl("gs://ciscoproject-6f757.appspot.com/").child("image");
         mAuth = FirebaseAuth.getInstance();
         mCurrentUser = mAuth.getCurrentUser();
         mDatabaseUsers = FirebaseDatabase.getInstance().getReference().child("Users").child(mCurrentUser.getUid());
@@ -65,11 +66,13 @@ public class PostActivity extends AppCompatActivity {
                 final String PostDesc = textDesc.getText().toString().trim();
                 // do a check for empty fields
                 if (!TextUtils.isEmpty(PostDesc) && !TextUtils.isEmpty(PostTitle)){
-                    StorageReference filepath = storage.child("post_images").child(uri.getLastPathSegment());
-                    filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    StorageReference childRef = storage.child("post_images");
+                    UploadTask uploadTask = childRef.putFile(filePath);
+
+                    uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                         @Override
                         public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                            @SuppressWarnings("VisibleForTests")
+
                             //getting the post image download url
                             final Uri downloadUrl = taskSnapshot.getUploadSessionUri();
                             Toast.makeText(getApplicationContext(), "Succesfully Uploaded", Toast.LENGTH_SHORT).show();
